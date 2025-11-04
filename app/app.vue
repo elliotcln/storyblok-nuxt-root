515fa416030e73580f003a40864466398bbaf32f
Elliot <elliot.cln@gmail.com>
2025-11-04 17:25:53 +0100

    Init: add Reka UI + Tailwindcss + Storyblok modules

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div></div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@storyblok/nuxt", "reka-ui/nuxt", "@nuxtjs/tailwindcss"],
+
+  storyblok: {
+    accessToken: "storyblok_public_key",
+    apiOptions: {
+      region: "eu",
+    },
+  },
+});
