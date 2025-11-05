708cbf7d7f1ac8bdc0e9750d20ccc5f7845557a9
Elliot <elliot.cln@gmail.com>
2025-11-05 14:15:47 +0100

    CI: add DaisyUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@storyblok/nuxt", "reka-ui/nuxt", "@nuxtjs/tailwindcss"],
+  modules: ["@storyblok/nuxt", "reka-ui/nuxt"],
+  css: ["~/assets/css/app.css"],
+  vite: { plugins: [tailwindcss()] },
 
   storyblok: {
     accessToken: "storyblok_public_key",
