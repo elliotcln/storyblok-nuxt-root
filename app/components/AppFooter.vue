8dcd203176da840bd18bd9aac5a399f6024a127c
Elliot <elliot.cln@gmail.com>
2025-11-06 13:46:26 +0100

    CI: ad homepage content + update navigations

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -6,9 +6,33 @@
         Copyright © {{ new Date().getFullYear() }}
       </p>
     </template>
+    <UNavigationMenu :items="footerLinks" variant="link" />
+    <template #right>
+      <UButton
+        icon="stash:social-facebook-solid"
+        color="neutral"
+        variant="ghost"
+        to="/"
+        target="_blank"
+        aria-label="Facebook"
+      />
+      <UButton
+        icon="ant-design:instagram-filled"
+        color="neutral"
+        variant="ghost"
+        to="https://instagram.com/roamwithelliot"
+        target="_blank"
+        aria-label="Instagram"
+      />
+    </template>
   </UFooter>
 </template>
 
-<script setup></script>
+<script setup>
+const footerLinks = [
+  { label: "Privacy Policy", to: "/privacy" },
+  { label: "Terms of Service", to: "/terms" },
+];
+</script>
 
 <style lang="scss" scoped></style>
--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,13 +1,29 @@
 <template>
   <UHeader title="Nuxt & Storyblok Starter">
+    <UNavigationMenu :items="links" />
+
     <template #right>
-      <UButton to="/">Sign In</UButton>
+      <UButton to="/" class="hidden lg:inline-flex" icon="stash:signin-duotone"
+        >Sign In</UButton
+      >
     </template>
 
-    <template #body></template>
+    <template #body>
+      <UNavigationMenu :items="links" orientation="vertical" />
+      <USeparator class="py-4" />
+      <UButton class="w-full justify-center" to="/" icon="stash:signin-duotone"
+        >Sign In</UButton
+      >
+    </template>
   </UHeader>
 </template>
 
-<script setup></script>
+<script setup>
+const links = [
+  { label: "Home", to: "/" },
+  { label: "About", to: "/about" },
+  { label: "Contact", to: "/contact" },
+];
+</script>
 
 <style lang="scss" scoped></style>
