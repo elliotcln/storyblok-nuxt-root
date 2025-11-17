c2ed085dcace710a623b670b919feebf7d5e4231
Elliot <elliot.cln@gmail.com>
2025-11-17 18:20:09 +0100

    feat: add faq component

---
+++ app/storyblok/faq-section.vue
@@ -0,0 +1,28 @@
+<template>
+  <Accordion
+    v-editable="blok"
+    class="mx-auto w-full max-w-2xl"
+    type="single"
+    collapsible
+    default-value="0-faq"
+  >
+    <AccordionItem
+      v-for="(question, index) in blok.questions"
+      :key="question.uid"
+      :value="index + '-faq'"
+    >
+      <AccordionTrigger class="text-md cursor-pointer">{{
+        question.title
+      }}</AccordionTrigger>
+      <AccordionContent class="text-primary/50">
+        <div v-html="question.content"></div>
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
+
+<style lang="scss" scoped></style>
